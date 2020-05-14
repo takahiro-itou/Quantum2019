60506209fdffd0ac484890175c90397bedb361b0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-05-14 22:34:48 +0900

    テンプレートをコピー

---
+++ Warmup/G2/Driver.cs
@@ -0,0 +1,20 @@
+using   System;
+
+using   Microsoft.Quantum.Simulation.Core;
+using   Microsoft.Quantum.Simulation.Simulators;
+
+namespace  Solution  {
+
+class  Driver
+{
+
+    static  void  Main(string[] args)
+    {
+        using (var qsim = new QuantumSimulator()) {
+            var result1 = Test1.Run(qsim).Result;
+            var result2 = Test2.Run(qsim).Result;
+        }
+    }
+}
+
+}
